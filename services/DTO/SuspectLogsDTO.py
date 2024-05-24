3d6c8a50a4f434cc75c0ebfe744b65500f597f08
Igor Barros Lopes de Mello <igorblopes1993@gmail.com>
2024-05-24 14:49:41 -0300

    Renomeando os arquivos

--- services/DTO/SuspectLogsDTO.py
+++ services/DTO/SuspectLogsDTO.py
@@ -3,12 +3,16 @@ from datetime import datetime
 class SuspectLogs():
     description: str
     type: str
+    namespace: str
+    ip: str
     timeCheck: datetime
     
-    def __init__(self, description:str, type: str):
+    def __init__(self, description:str, type:str, namespace: str, ip: str):
         self.description = description
         self.type = type
+        self.namespace = namespace
+        self.ip = ip
         self.timeCheck = datetime.now()
         
     def toJson(self):
-        return {"description": self.description, "type": self.type, "timeCheck": self.timeCheck.strftime('%d/%m/%Y %H:%M:%S')}
+        return {"description": self.description, "type": self.type, "namespace": self.namespace, "ip": self.ip, "timeCheck": self.timeCheck.strftime('%d/%m/%Y %H:%M:%S')}
