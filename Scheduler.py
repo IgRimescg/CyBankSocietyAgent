f446719b05069985c6360bb47c5026987c37e6b2
Igor Barros <igorblopes1993@gmail.com>
2024-05-31 21:14:52 -0300

    Acertando requirements

--- Scheduler.py
+++ Scheduler.py
@@ -8,7 +8,7 @@ from core import start_checks
 
 tl = Timeloop()
 
-@tl.job(interval=timedelta(minutes=1))
+@tl.job(interval=timedelta(minutes=5))
 def check_every_5s():
     start_checks.star_check()
     about_service.update_about_last_check(datetime.now())
